4efa27ca011507c3d7147cc55af76e9e2cfb8d82
Piper McCorkle <contact@piperswe.me>
2026-01-03 17:03:26 -0600

    lock each march separately

--- process.sh
+++ process.sh
@@ -11,24 +11,23 @@ sha256file() {
 
 cd ~
 
-incoming_dir="${1:-incoming}"
+incoming_dir="$1"
+march="$2"
 
-lockfile ~/process.lock
-trap "rm -f ~/process.lock" EXIT
+lockfile ~/process-$march.lock
+trap "rm -f ~/process-$march.lock" EXIT
 
-for march in $(cat ~/marches.txt)
-do
-        mkdir -p $incoming_dir/$march perpetuity/$march/{pool,dists/trixie/core/binary-i386}
-        pushd perpetuity/$march
-        find ~/$incoming_dir/$march -name '*.deb' -exec cp '{}' pool/ \;
-        find ~/$incoming_dir/$march -name '*.udeb' -exec cp '{}' pool/ \;
-        find ~/$incoming_dir/$march -type f -delete
-        dpkg-scanpackages pool/ > dists/trixie/core/binary-i386/Packages
-        gzip -kf dists/trixie/core/binary-i386/Packages
-        xz -kf dists/trixie/core/binary-i386/Packages
-        bzip2 -kf dists/trixie/core/binary-i386/Packages
-        pushd dists/trixie
-        cat > Release <<EOF
+mkdir -p ~/$incoming_dir perpetuity/$march/{pool,dists/trixie/core/binary-i386}
+pushd perpetuity/$march
+find ~/$incoming_dir -name '*.deb' -exec cp '{}' pool/ \;
+find ~/$incoming_dir -name '*.udeb' -exec cp '{}' pool/ \;
+find ~/$incoming_dir -type f -delete
+dpkg-scanpackages pool/ > dists/trixie/core/binary-i386/Packages
+gzip -kf dists/trixie/core/binary-i386/Packages
+xz -kf dists/trixie/core/binary-i386/Packages
+bzip2 -kf dists/trixie/core/binary-i386/Packages
+pushd dists/trixie
+cat > Release <<EOF
 Origin: Perpetuity
 Label: Perpetuity
 Suite: trixie
@@ -37,18 +36,17 @@ Codename: trixie
 Architectures: all i386
 Components: core
 MD5Sum:
- $(md5file core/binary-i386/Packages)
- $(md5file core/binary-i386/Packages.gz)
- $(md5file core/binary-i386/Packages.xz)
- $(md5file core/binary-i386/Packages.bz2)
+$(md5file core/binary-i386/Packages)
+$(md5file core/binary-i386/Packages.gz)
+$(md5file core/binary-i386/Packages.xz)
+$(md5file core/binary-i386/Packages.bz2)
 SHA256:
- $(sha256file core/binary-i386/Packages)
- $(sha256file core/binary-i386/Packages.gz)
- $(sha256file core/binary-i386/Packages.xz)
- $(sha256file core/binary-i386/Packages.bz2)
+$(sha256file core/binary-i386/Packages)
+$(sha256file core/binary-i386/Packages.gz)
+$(sha256file core/binary-i386/Packages.xz)
+$(sha256file core/binary-i386/Packages.bz2)
 EOF
-        popd
-        gpg -a -s --clearsign < dists/trixie/Release > dists/trixie/InRelease
-        gpg -a -b -s < dists/trixie/Release > dists/trixie/Release.gpg
-        popd
-done
+popd
+gpg -a -s --clearsign < dists/trixie/Release > dists/trixie/InRelease
+gpg -a -b -s < dists/trixie/Release > dists/trixie/Release.gpg
+popd
